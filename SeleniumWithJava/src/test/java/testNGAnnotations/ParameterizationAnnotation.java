876ea667d5f2a7b9a6f365bdc1611755bff070d4
Testriq_D25 <Testriq_D25@DESKTOP-OF3OC9C>
2023-08-10 11:25:05 +0530

    TestNgAnnotations

---
+++ SeleniumWithJava/src/test/java/testNGAnnotations/ParameterizationAnnotation.java
@@ -0,0 +1,23 @@
+package testNGAnnotations;
+
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ParameterizationAnnotation {
+
+	@Parameters({"URL"})
+	@Test
+	
+	public void LaunchBrowser(String url) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver= new ChromeDriver();
+		driver.manage().window().maximize();
+			driver.get(url);
+		
+	}
+}
+
