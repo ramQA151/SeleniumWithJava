aa25fe1714d8931adc344c459208eecd6002467f
Testriq_D25 <Testriq_D25@DESKTOP-OF3OC9C>
2023-08-10 15:08:19 +0530

    Tested Assertion for Filpkart

--- SeleniumWithJava/src/test/java/testNGAnnotations/ParameterizationAnnotation.java
+++ SeleniumWithJava/src/test/java/testNGAnnotations/ParameterizationAnnotation.java
@@ -1,5 +1,6 @@
 package testNGAnnotations;
 
+import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import org.openqa.selenium.WebDriver;
@@ -10,13 +11,19 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class ParameterizationAnnotation {
 
 	@Parameters({"URL"})
-	@Test(enabled=false)
+	@Test
 	
 	public void LaunchBrowser(String url) {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver= new ChromeDriver();
 		driver.manage().window().maximize();
 			driver.get(url);
+			String ActTital=driver.getTitle();
+			System.out.println(ActTital);
+			
+			String ExpectedTitle="Online Shopping Site for Mobiles, Electronics, Furniture, Grocery, Lifestyle, Books & More. Best Offers!";
+			Assert.assertEquals(ActTital, ExpectedTitle);
+			
 		
 	}
 }
